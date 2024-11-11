327cf9f300a054466ca6cefb1c47288f258f96e6
daquintero <darioaquintero@gmail.com>
2024-11-11 17:54:34 +0100

    Ok this is improving, now need to find a way to run all examples

--- scripts/test_coverage.sh
+++ scripts/test_coverage.sh
@@ -2,7 +2,13 @@
 coverage run -m pytest tests/
 
 # List of example scripts
-examples=("03_sax_basics.py" )
+examples=(
+    "03_sax_basics.py" \
+    "03a_sax_cocotb_cosimulation.py" \
+    "03b_optical_function_verification.py" \
+    "04_spice_cosimulation.py" \
+#    "06_component_codesign_basics.py" \
+)
 
 # Run each example script with coverage, appending to the same coverage data
 for example in "${examples[@]}"; do
