011ed3959b0076f40fd5eecf65d7f08f7e2699f3
daquintero <darioaquintero@gmail.com>
2024-11-12 11:32:40 +0100

    status

--- scripts/test_coverage.sh
+++ scripts/test_coverage.sh
@@ -1,15 +1,15 @@
 # Run from base directory
 export COVERAGE_FILE="$(pwd)/.coverage"
+# export CONTINUE_ON_FAILURE=true
 echo "COVERAGE_FILE: ${COVERAGE_FILE}"
 rm -f $COVERAGE_FILE
 coverage run --branch -m pytest tests/
 
 # List of example scripts
 examples=(
-    "03_sax_basics.py"
-    "03a_sax_cocotb_cosimulation.py"
-    "03b_optical_function_verification.py"
-    "04_spice_cosimulation.py"
+#    "03_sax_basics.py"
+#    "03a_sax_cocotb_cosimulation.py"
+#    "03b_optical_function_verification.py"
     "04_spice_cosimulation/04_spice_cosimulation.py"
     "04b_rf_stages_performance.py"
     "05_quantum_integration_basics.py"
@@ -38,7 +38,18 @@ for example in "${examples[@]}"; do
 
     # Run the script with coverage
     coverage run --branch --append "$example_file"
+    if [ $? -ne 0 ]; then
+        echo "Example ${example} failed."
+        failed_examples+=("$example")
+        # Exit immediately if CONTINUE_ON_FAILURE is not set
+        if [ -z "$CONTINUE_ON_FAILURE" ]; then
+            echo "Exiting on first failure."
+            exit 1
+        fi
+    fi
     echo "Appending coverage to: ${COVERAGE_FILE}"
+
+
     # total=$(coverage report | tail -n1 | awk '{print $NF}' | tr -d '%')
     # echo "### Total coverage: ${total}%"
 
@@ -58,3 +69,13 @@ coverage combine
 
 # Generate the coverage report
 coverage report -m
+
+# Display any failed examples if CONTINUE_ON_FAILURE was set
+if [ ${#failed_examples[@]} -ne 0 ]; then
+    echo -e "\nThe following examples failed:"
+    for example in "${failed_examples[@]}"; do
+        echo "- $example"
+    done
+    # Exit with a non-zero code to indicate failure if there were any errors
+    exit 1
+fi
